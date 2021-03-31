57c628272557d15daec5a58584d5c5f51c1c9def
Ivan <irodkin2002@gmail.com>
2021-03-31 22:21:09 +0300

    "dick" variable removed and code style fixed

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dick = pickle.load(fo, encoding='bytes')
-    return dick
+n = torch.randn(20, 30, 10, 10)
+m = nn.Conv2d(30, 40, (5, 5))
+p = nn.MaxPool2d(2, 2)
+print(m(n).size())
 
 
-data_dir = 'cifar-100-python'
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -26,24 +23,28 @@ testset = torchvision.datasets.CIFAR100(root='./data', train=False,
                                         download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                          shuffle=False, num_workers=2)
-kerSz = 5
-ker = (kerSz, kerSz)
 
 
 class Net(nn.Module):
+    c1 = 6
+    c2 = 18
+    kerSz = 5
+    ker = (kerSz, kerSz)
+    pl = 2
+    l1 = 120
 
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, ker)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, ker)
-        self.fc1 = nn.Linear(16 * kerSz**2, 120)
-        self.fc2 = nn.Linear(120, 100)
+        self.conv1 = nn.Conv2d(3, self.c1, self.ker)
+        self.pool = nn.MaxPool2d(self.pl, self.pl)
+        self.conv2 = nn.Conv2d(self.c1, self.c2, self.ker)
+        self.fc1 = nn.Linear(self.c2 * (((32 - self.kerSz + 1) // self.pl - self.kerSz + 1) // self.pl) ** 2, self.l1)
+        self.fc2 = nn.Linear(self.l1, 100)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * kerSz**2)
+        x = x.view(-1, self.c2 * (((32 - self.kerSz + 1) // self.pl - self.kerSz + 1) // self.pl) ** 2)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return x
@@ -53,7 +54,6 @@ net = Net()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
 
 for epoch in range(4):  # loop over the dataset multiple times
 
@@ -73,7 +73,7 @@ for epoch in range(4):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
+        if i % 2000 == 1999:  # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
